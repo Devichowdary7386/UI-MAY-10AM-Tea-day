e8d3f9d61fcbaca386f582996f98ba1969834da8
Devichowdary7386 <ygurudevi80@gmail.com>
2024-07-05 20:53:28 +0530

    okie

---
+++ Javascript/part 2/bitwise operator/one.js
@@ -0,0 +1,10 @@
+//given number is 3-digit number or not
+let num=451
+if( num>=100 &&num<=999){
+    console.log("yes")
+
+}
+else{
+    console.log("No")
+
+}
---
+++ Javascript/part 2/iterative/five.js
@@ -0,0 +1,3 @@
+let eid=101
+let ename="Rahul"
+console.log(`Emp id:${eid} and name:${ename}`)
