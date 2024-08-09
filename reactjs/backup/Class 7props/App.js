6884e6bf088dac8965e01c16afecd83063f3c5df
Devichowdary7386 <ygurudevi80@gmail.com>
2024-08-09 21:58:38 +0530

    test

---
+++ reactjs/backup/Class 7props/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Product from './Components/Product'
+const App = () => {
+  return <div>
+            <h2>App Component</h2>
+            <hr/>
+            <Product/>
+         </div>
+}
+
+export default App
